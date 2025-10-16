bdd33fbb7ffcb9d595ea71727678ac2eea1d61ef
Evan Vang <evankvang@gmail.com>
2025-10-16 02:24:35 -0500

    Added javascript to add tasks to the page. And yet another design change.

--- src/index.js
+++ src/index.js
@@ -1,39 +1,81 @@
+/*
+There should only be one task with a specific priority
+Meaning for example only one task with priority 1.
+*/
+
 let tasks = [];
 
 function addTask(taskPriority, taskName) {
-    let newTask = { priorityNumber: taskPriority, nameOfTask: taskName };
+  taskPriority = parseInt(taskPriority);
+
+  if (!verifyPriority(tasks, taskPriority)) {
+    window.alert('Please do not use duplicate priority numbers.');
+    return;
+  }
+
+  let newTask = { priorityNumber: taskPriority, nameOfTask: taskName };
+
+  tasks.push(newTask);
 
   const taskDisplayArea = document.getElementById('taskArea');
 
   let divForTask = document.createElement('div');
   let h1ForTask = document.createElement('h1');
+  let removeButton = document.createElement('button');
+
+  //TODO: 
+  // Come up with a way to edit the tasks.
 
   h1ForTask.textContent = `${newTask.priorityNumber}: ${newTask.nameOfTask}`;
 
+  removeButton.textContent = 'Remove Task';
+  removeButton.onclick = function () {
+    removeTask(newTask.priorityNumber);
+  };
+
   divForTask.append(h1ForTask);
+  divForTask.append(removeButton);
   divForTask.className = 'task';
   divForTask.style.display = 'block';
   taskDisplayArea.append(divForTask);
+
+  reorderList();
 }
 
+function verifyPriority(taskList, input) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].priorityNumber === input) {
+      return false;
+    }
+    return true;
+  }
+  return true;
+}
 
-function redrawTasksArea(listOfTasks) {
-  const taskDisplayArea = document.getElementById('taskArea');
+function reorderList() {
+  for (let i = 0; i < tasks.length; i++) {
+    for (let j = i + 1; j < tasks.length; j++) {
+      if (tasks[i].priorityNumber > tasks[j].priorityNumber) {
+        let temp = tasks[i];
+        tasks[i] = tasks[j];
+        tasks[j] = temp;
+      }
+    }
+  }
+}
 
-  // TODO: fix the weird cloning that happend when you keep adding tasks.
-  const oldListArea = taskDisplayArea.getElementsByTagName('div');
-  for (let i = 0; i < oldListArea.length; i++) {
-    oldListArea[i].remove();
+function removeTask(input) {
+  let listOfTasks = document.querySelectorAll('.task');
+
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].priorityNumber === input) {
+      tasks.splice(i, 1);
+    }
   }
 
   listOfTasks.forEach((t) => {
-    let divForTask = document.createElement('div');
-    let h1ForTask = document.createElement('h1');
-
-    h1ForTask.textContent = `${t.priorityNumber}: ${t.nameOfTask}`;
-
-    divForTask.append(h1ForTask);
-    divForTask.className = 'task';
-    taskDisplayArea.append(divForTask);
+    if (t.textContent.startsWith(input, 0)) {
+      t.remove();
+    }
   });
 }
